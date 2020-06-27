2613836235d095e2246991dba2a2a73fa382bd84
Anis KCHAOU <kchaouanis25@gmail.com>
2020-06-27 19:02:55 +0200

    Update CinemaController.java

--- src/main/java/fr/uha/anis/controller/CinemaController.java
+++ src/main/java/fr/uha/anis/controller/CinemaController.java
@@ -23,7 +23,7 @@ import fr.uha.anis.dao.FilmRepository;
 import fr.uha.anis.dao.TicketRepository;
 import fr.uha.anis.entities.Film;
 import fr.uha.anis.entities.Ticket;
-@CrossOrigin("www.cinemas-anis-kchaou-fullstack.herokuapp.com")
+@CrossOrigin("www.cinemas-anis-kchaou-fullstack.herokuapp.com:8080")
 @RestController
 
 public class CinemaController {
